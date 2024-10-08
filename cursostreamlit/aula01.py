34ad182bb3fc89a05cfedc5a0cf851859f50ace4
rafaelflorentinobarbosa <rafael.florentino.barbosa@gmail.com>
2024-10-08 11:57:00 -0300

    curso streamlit

---
+++ cursostreamlit/aula01.py
@@ -0,0 +1,11 @@
+# No prompt instale: 
+# pip install streamlit
+# pip install plotly.express
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+
+st.title("Dashboard de Vendas:shopping_trolley: ")
+
+# streamlit run aula01.py
