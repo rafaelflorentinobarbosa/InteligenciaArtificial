839be2cc76ac8ccdd950a87f70221b696ff9523a
rafaelflorentinobarbosa <rafael.florentino.barbosa@gmail.com>
2024-11-07 11:50:10 -0300

    ajuste aula5

--- redes_neurais/aplicativo_aula4.py
+++ redes_neurais/aplicativo_aula4.py
@@ -1,43 +1,56 @@
-# Objetivos da Aula 4
-
 #importação do opencv-python
 import cv2
 import mediapipe as mp # importação do mediapipe para usar o facemesh
-import numpy as np
+import numpy as np # função EAR
 
-# Entender as cordenadas do mediaPipe
-# Entender a normalização e desnormalização dos pontos do MediaPipe
-# Analisar os olhos
-# Utilizar a função EAR (Distância euclidiana) - segundo o artigo
+# Objetivos da Aula 4
+# Entender as Coordenadas do MediaPipe
+# Entender a normalização e desnormalização do pontos do MediaPipe
+# Analisar os olhos (seguindo o artigo)
+# Utilizar a função EAR (distância euclidiana) - seguindo o artigo
 
-# Pontos dos olhos
-p_olho_esq = [385, 380, 387, 373, 362, 263]
-p_olho_dir = [160, 144, 158, 153, 33, 133]
+# pontos dos olhos
+# olho esquerdo
+p_olho_esq = [385,380,387,373,362,263]
 
-# Funcção  que calcula EAR
-def calculo_ear(face, p_olho_dir, p_olho_esq):
-    # FIXME: List comprehension
-    # [[]] - lista dentro de outra lista
-    # tente
-    try:    
-        # 1 - face é uma variável que vai resceber as cordenadas
-        # 2 - np.array()
-        # 3 - List comprehension - for
-        # 4 - face (numpy) --- in face (mediapipe)
+# olho direito
+p_olho_dir = [160,144,158,153,33,133]
+p_olhos = p_olho_esq+p_olho_dir
+#FIXME: 06/11 - continuação (Imersão Euclidiana)
+# função EAR
+
+# uma função que calcula o EAR
+def calculo_ear(face,p_olho_dir,p_olho_esq):
+    #FIXME: list comprehension
+    #[[]] - lista dentro de outra lista
+    #tente
+    try:
+        """
+        1 -face é uma variável que vai receber as coordenadas
+        2 -np.array()
+        3 - List comprehension - for
+        4 - face (numpy)  ---  in face (mediapipe)
+        """
         
-        face = np.array([[coord.x, coord.y] for coord in face])
-        # matriz (array) Linhas e colunas
+        face = np.array([[coord.x,coord.y] for coord  in face] )
+        # matriz (array) - linhas e colunas [linhas,colunas]
+        # face_esq
         face_esq = face[p_olho_esq, :]
+        # face_dir
         face_dir = face[p_olho_dir, :]
-        
+        # algebra linear  - geometria - estatistica - probabilidade
         ear_esq = (np.linalg.norm(face_esq[0] - face_esq[1]) + np.linalg.norm(face_esq[2] - face_esq[3])) / (2 * (np.linalg.norm(face_esq[4] - face_esq[5])))
         ear_dir = (np.linalg.norm(face_dir[0] - face_dir[1]) + np.linalg.norm(face_dir[2] - face_dir[3])) / (2 * (np.linalg.norm(face_dir[4] - face_dir[5])))
     except:
+        # zerando
         ear_esq = 0.0
         ear_dir = 0.0
-        
+    #fórmula
     media_ear = (ear_esq + ear_dir) / 2
     return media_ear
+
+
+
 
 
 #criar uma variável para camera
@@ -58,9 +71,11 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
         if not sucesso:
             print("ignorando o frame vazio da camêra")
             continue
+        comprimento,largura,_=frame.shape
         # transformando de BGR para RGB
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        # FIXME: criar uma variável, dados processados (ex.pontos do rosto), saida_facemesh
+        # criar uma variável      dados processados (ex.pontos do rosto)
+        #FIXME: processamento do frame (saida_facemesh)
         saida_facemesh = facemesh.process(frame)
         # O OpenCV - entende BGR
         frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
@@ -73,8 +88,8 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
             # saida_facemesh é o nosso conjunto de coordenadas
             # o multi_face_landmarks vai retornas as coordenadas
             # após isso ele deve desenhar esses pontos no nosso rosto
-            #FIXME: acesso as cordenadas (multi_face_landmarks)
-            # Traz as cordenadas da malha 3D (x,y,z)
+            #FIXME: acesso as coordenadas (multi_face_landmarks)
+            # traz as coordenadas da malha 3D (x,y,z)
             for face_landmarks in saida_facemesh.multi_face_landmarks:
                 # desenhar
                 # uso o método draw_landmarks para pontuar os desenhos
@@ -82,32 +97,28 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
                 # o nosso (frame)
                 # as coordenadas - (face_landmarks)
                 # Especificar os nossos pontos : FACEMESH_CONTOURS
-                mp_drawing.draw_landmarks(frame,face_landmarks,
-                mp_face_mesh.FACEMESH_CONTOURS,
-                landmark_drawing_spec = mp_drawing.DrawingSpec(color=(255,202,102),thickness=1,circle_radius=1),
-                connection_drawing_spec = mp_drawing.DrawingSpec(color=(102,204,0),thickness=1,circle_radius=1))
+                # tickness = grossura
+                mp_drawing.draw_landmarks(frame, 
+                                    face_landmarks, 
+                                    mp_face_mesh.FACEMESH_CONTOURS,
+                                    landmark_drawing_spec = mp_drawing.DrawingSpec(color=(255,102,102),thickness=1,circle_radius=1),
+                                    connection_drawing_spec = mp_drawing.DrawingSpec(color=(102,204,0),thickness=1,circle_radius=1)) 
                 
-            #FIXME: Normalização para pixel
-            
-
-            # face = face_landmarks.landmark
-            # for id_coord, coord_xyz in enumerate(face): .
-            #     if id_coord in p_olhos:
-            #         coord_cv = mp_drawing._normalized_to_pixel_coordinates
-            #         cv2.circle(frame,coord_cv,2,(255,0,0), -1)
-            
-
-            
-            #FIXME: Chamada do EAR e print
-            
-            # mostrando o EAR na tela
-            #  cv2.rectangle(frame, (0,1),(290,140),(58,58,55),-1)
-            #                 cv2.putText(frame, f"EAR: {round(ear, 2)}", (1, 24),
-            #                                 cv2.FONT_HERSHEY_DUPLEX,
-            #                                 0.9, (255, 255, 255), 2)
-
-            # {documentação do OpenCV}
-            # https://docs.opencv.org/4.x/d6/d6e/group__imgproc__draw.html
+                #FIXME: normalização para pixel
+                face  = face_landmarks.landmark
+                for id_coord,coord_xyz in enumerate(face):
+                    if id_coord in p_olhos:
+                        coord_cv=mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x,coord_xyz.y,largura,comprimento)
+                        cv2.circle(frame,coord_cv,2,(255,0,0),-1)
+                #FIXME: chamada do EAR e print
+                ear = calculo_ear(face,p_olho_dir,p_olho_esq)
+                # mostrando o EAR na tela
+                # criando um retangulo cinza solido  (-1)
+                cv2.rectangle(frame, (0,1),(290,140),(58,58,55),-1)
+                # mostrando os valores no frame
+                cv2.putText(frame, f"EAR: {round(ear, 2)}", (1, 24),
+                                cv2.FONT_HERSHEY_DUPLEX,
+                                0.9, (255, 255, 255), 2)
 
         except:
             print("algo deu errado")
